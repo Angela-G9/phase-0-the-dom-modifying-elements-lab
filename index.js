1bb646f50e6c8a0a1a90f3c25128d5746a9d6d89
Angela-G9 <angelagathoni9568@gmail.com>
2024-12-09 23:04:23 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// Step 2: Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Step 3: Set the id of the new <h1> element to 'victory'
+newHeader.id = "victory";
+
+// Step 4: Set the text content of the new <h1> element
+newHeader.textContent = "Angela is the champion";
+
+// Append the new <h1> element to the document body
+document.body.append(newHeader);
